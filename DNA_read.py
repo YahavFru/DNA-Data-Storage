f74d33f718ba67b60b8684aaebdc8591fe630f07
YahavFru <fruyahav@gmail.com>
2024-06-21 16:55:27 +0300

    Remade RegEx pam finding and encode function

--- DNA_read.py
+++ DNA_read.py
@@ -1,68 +1,58 @@
+import re
+
+is_print = True
+
+def pam_finder(dna_seq, pam):
+    pam_indices = re.finditer('.GG', dna_seq)
+    return [pam.end() for pam in pam_indices] # Returns the start position of each pam sequence
+    
 # TODO: a function that finds the PAMs
 # input: sequence + pam
 # output: a list of pam indices 
 
 def main(dna_seq, pam):
-    enumerated_dna_seq = enumerate(dna_seq)
-    pam_end_indexes = []
-    # TODO: Replace with regex
-    pam_match_counter = 0
-    for base in enumerated_dna_seq:
-        is_pam, pam_match_counter = pam_checker(base[1], pam, pam_match_counter)
-        if is_pam:
-            pam_match_counter += 1
-            if pam_match_counter == len(pam):
-                pam_end_indexes.append(base[0] +1)
-                pam_match_counter = 0
-        else:
-            pam_match_counter = 0
+    pam_indices = pam_finder(dna_seq, pam)
+    if is_print:
+        print(f'The PAM indeces are: ${pam_indices}, The PAM sequences are: ${[dna_seq[pam_index] for pam_index in pam_indices]}')
+    dna_data_storage_proccess.__init__(dna_data_storage_proccess, dna_seq, pam_indices)    
+    dna_data_storage_proccess.write(dna_data_storage_proccess)
 
-    print(f'Pam indexes in DNA: {pam_end_indexes}')
-    encoder.__init__(encoder, dna_seq, pam_end_indexes)    
-    encoder.write(encoder)
-
-# TODO: Replace with regex
-def pam_checker(base, pam, pam_index): #Returns: Is part of PAM? + what part of PAM?
-    if pam[pam_index] == 'N' or base == pam[pam_index]:
-        return True, pam_index
-    
-    elif pam_index != 0:
-        return pam_checker(base, pam, 0)
-    
-    return False, -1
 
 # TODO: joint class for encoder+writer+reader+decoder
 # Shared metadata: DNA seq, PAM indices (or at least PAM sequence + logic to generate a list of indices)
 # functions:
-# encoder: binary message -> "ideal" edited sequence/list of PAM indices to edit
+# encoder: binary message -> "ideal" edited sequence/list of PAM indices to edit (V)
 # writer: simulate the enzymatic process. DNA seq + list of PAM indices to edit -> list of *many* edited DNA sequence
 #         parameters: edit probability, number of copies, other noise
 # reader: simulate NGS. list of *many* edited DNA sequence + DNA seq (unedited) -> list of edited PAM indices
 #         parameters: logic for identifying editing from a noisy signal
 # decoder: "ideal" edited sequence/list of PAM indices to edit -> binary message
 
-class encoder:
+class dna_data_storage_proccess:
     bit_list = [1] #array of booleans / 0s or 1s
-    dna_seq = [] #original DNA sequence, array of single char string
+    dna_seq = '' #original DNA sequence, string
+    tagged_ideal_seq = ''
+    grna_indexes_for_edit = []
     grna_indexes = [] #array of indexes for each gRNA block on DNA
     tagged_dna_seqs = [] #array of dna seqs, each edited.
 
-    def __init__(self, origin_dna, pam_end_indexes): #Defines all inputs for the class
-        for i in range(10):
-            self.tagged_dna_seqs.append(origin_dna)
-        for index in pam_end_indexes:
+    def __init__(self, origin_dna, pam_end_indices): #Defines all inputs for the class
+        self.dna_seq = origin_dna
+        for index in pam_end_indices:
             self.grna_indexes.append(index)
+        
+    def encode(self):
         if len(self.bit_list) != len(self.grna_indexes):
-            print(f'Bit list length ({len(self.bit_list)}) does not match the number of pam sequences present ({len(self.grna_indexes)})')
+            print(f'Error: Bit list length ({len(self.bit_list)}) does not match the number of pam sequences present ({len(self.grna_indexes)})')
+        else:
+            for index in self.grna_indexes:
+                if self.bit_list[index]: # is this bit 1? i.e, does it require editing?
+                    self.grna_indexes_for_edit.append(index)
+                    for base_jump in range(20):
+                        if self.dna_seq[index + base_jump] == 'C':
+                            self.tagged_ideal_seq += 'T'
+                        else: 
+                            self.tagged_ideal_seq += self.dna_seq[index + base_jump]
 
-    def write(self):
-        grna_counter = 0
-        for dna_seq in self.tagged_dna_seqs:
-            for grna in self.grna_indexes:
-                if self.bit_list[grna_counter]:
-                    for base in range(20):
-                        if dna_seq[grna + base] == 'C': 
-                            del dna_seq[grna + base]
-                            dna_seq.insert(grna + base, 'T') # This is the part where the DNA gets "Tagged", assuming 100% success rate
-        print(self.tagged_dna_seqs[9])
-main(['A','G','G','T','C','C','C','C','C','C','C','C','G','A','C','C','C','C','C','C','C','C','G'], 'NGG')
+
+main('AGGCCCCCCCCCCCCCCCCCCCC', 'NGG')
